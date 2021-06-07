7c437cc7110ca2ff7c9aed1f58eb45029a509779
Daniel <daniel.herzig@outlook.at>
2021-06-08 01:02:30 +0200

    removed whitespace

--- multimedia_config.scm
+++ multimedia_config.scm
@@ -30,22 +30,22 @@
  (keyboard-layout (keyboard-layout "de"))
  (host-name "laptop")
  (users (cons* (user-account
-                (name "daniel")
-                (comment "Daniel")
-                (group "users")
-                (home-directory "/home/daniel")
-                (supplementary-groups
-                 '("wheel" "netdev" "audio" "video")))
-               %base-user-accounts))
+		(name "daniel")
+		(comment "Daniel")
+		(group "users")
+		(home-directory "/home/daniel")
+		(supplementary-groups
+		 '("wheel" "netdev" "audio" "video")))
+	       %base-user-accounts))
  (packages (append (map specification->package
 			'("nss-certs"))
 		   %base-packages))
  (services
   (append
    (list (service gnome-desktop-service-type)
-         (set-xorg-configuration
-          (xorg-configuration
-           (keyboard-layout keyboard-layout))))
+	 (set-xorg-configuration
+	  (xorg-configuration
+	   (keyboard-layout keyboard-layout))))
    %desktop-services))
  (bootloader
   (bootloader-configuration
@@ -54,18 +54,18 @@
    (keyboard-layout keyboard-layout)))
  (mapped-devices
   (list (mapped-device
-         (source
-          (uuid "e3abd143-5cdd-438a-b316-ef7297aae979"))
-         (target "cryptroot")
-         (type luks-device-mapping))))
+	 (source
+	  (uuid "e3abd143-5cdd-438a-b316-ef7297aae979"))
+	 (target "cryptroot")
+	 (type luks-device-mapping))))
  (file-systems
   (cons* (file-system
-          (mount-point "/boot/efi")
-          (device (uuid "88A8-FC68" 'fat32))
-          (type "vfat"))
-         (file-system
-          (mount-point "/")
-          (device "/dev/mapper/cryptroot")
-          (type "ext4")
-          (dependencies mapped-devices))
-         %base-file-systems)))
+	  (mount-point "/boot/efi")
+	  (device (uuid "88A8-FC68" 'fat32))
+	  (type "vfat"))
+	 (file-system
+	  (mount-point "/")
+	  (device "/dev/mapper/cryptroot")
+	  (type "ext4")
+	  (dependencies mapped-devices))
+	 %base-file-systems)))
