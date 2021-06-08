cedaf36f65579699c76481b903b37defa4dfb09f
Daniel <daniel.herzig@outlook.at>
2021-06-09 00:26:47 +0200

    rearranging

--- multimedia_config.scm
+++ multimedia_config.scm
@@ -1,10 +1,11 @@
 ;; modified from the graphical GNU/GUIX installer (1.3.0)
-;; from http://guix.gnu.org/de/download/
-;; to stabilize multi-monitor behaviour
-;; with system upgrades. This is done by pinning a kernel
-;; visible from the initial installation guix-commit.
-;; mainly adapted from https://gitlab.com/nonguix/nonguix
-;; in the section 'avoiding kernel upgrades'
+;; from http://guix.gnu.org/de/download/ to stabilize
+;; multi-monitor behaviour with system upgrades.
+;; This is done by pinning a kernel visible from
+;; the initial installation guix-commit.
+;; the kernel pinning is mainly adapted from
+;; https://gitlab.com/nonguix/nonguix in the section
+;; 'avoiding kernel upgrades'
 
 (use-modules (gnu)
 	     (gnu packages)
